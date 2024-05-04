70d2074aa0e319c550b9a487e479d09f69e461fa
ki3mi <ccentemejiajosecarlos@gmail.com>
2024-05-03 23:54:08 -0500

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen justify-center items-center p-24">
+      <div className="flex justify-center items-center p-2 rounded-full font-bold"><p className="[text-shadow:_6px_-1px_12px_#08C2CE]">Hola Mundo</p></div>
+    </main>
+  );
+}
